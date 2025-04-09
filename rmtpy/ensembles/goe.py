fd34e84916e8c9147bc00ffead24e3d32b2663e8
Joshua Leeman <joshua.r.leeman@gmail.com>
2025-04-09 18:33:35 -0400

    Document GOE class

---
+++ rmtpy/ensembles/goe.py
@@ -0,0 +1,100 @@
+# rmtpy.ensembles.goe.py
+"""
+This module contains the programs defining the Gaussian Orthogonal Ensemble (GOE).
+It is grouped into the following sections:
+    1. Imports
+    2. Attributes
+    3. Ensemble Class
+"""
+
+# =============================
+# 1. Imports
+# =============================
+# Third-party imports
+import numpy as np
+
+# Local application imports
+from ._rmt import Tenfold
+
+
+# =============================
+# 2. Attributes
+# =============================
+# Class name for dynamic imports
+class_name = "GOE"
+
+# Dyson index
+beta = 1
+
+# Degeneracy of eigenvalues
+degeneracy = 1
+
+
+# =============================
+# 2. Ensemble Class
+# =============================
+class GOE(Tenfold):
+    def __init__(
+        self,
+        N: int = None,
+        dim: int = None,
+        scale: float = 1.0,
+        dtype: type = np.complex128,
+    ) -> None:
+        """
+        Initialize the Gaussian Orthogonal Ensemble (GOE).
+
+        Parameters
+        ----------
+        N : int, optional
+            Number of Majorana fermions
+        dim : int, optional
+            Dimension of the matrix
+        scale : float, optional
+            Energy scale (default is 1.0)
+        dtype : type, optional
+            Data type of the matrix elements (default is np.complex128)
+        """
+        # Initialize tenfold ensemble
+        super().__init__(N=N, dim=dim, scale=scale, dtype=dtype)
+
+        # Set degeneracy of eigenvalues
+        self._degeneracy = degeneracy
+
+    def generate(self, out: np.ndarray = None) -> np.ndarray:
+        """
+        Return a random matrix from the GOE.
+
+        Parameters
+        ----------
+        out : np.ndarray, optional
+            If provided, the generated matrix will be stored in this array.
+            Otherwise, a new array will be allocated.
+
+        Returns
+        -------
+        np.ndarray
+            Random matrix from the GOE.
+        """
+        # Allocate memory for GOE matrix
+        H = np.empty((self.dim, self.dim), dtype=self.dtype)
+
+        # Generate standard normal numbers for real parts and zeros for imaginary parts
+        H.real = self._rng.standard_normal((self.dim, self.dim), dtype=self.real_dtype)
+        H.imag = np.zeros((self.dim, self.dim), dtype=self.real_dtype)
+
+        # Symmetrize matrix in-place
+        np.add(H, H.T, out=H)
+
+        # Scale matrix in-place
+        H *= self.sigma / 2
+
+        # Return GOE matrix
+        return H
+
+    @property
+    def degeneracy(self) -> int:
+        """
+        Degeneracy of the ensemble's eigenvalues.
+        """
+        return self._degeneracy
