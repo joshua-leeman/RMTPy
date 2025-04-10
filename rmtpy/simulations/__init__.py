3bc4f32e366c7f640d4b5add9fefb72098d1cc4f
Joshua Leeman <joshua.r.leeman@gmail.com>
2025-04-10 16:14:18 -0400

    Create rmtpy/simulations/__init__.py

---
+++ rmtpy/simulations/__init__.py
@@ -0,0 +1,21 @@
+# rmtpy.simulations.__init__.py
+"""
+This __init__ file sets the environment variables to conduct HPC simulations
+for the RMTpy package.
+It is grouped into the following sections:
+    1. Imports
+    2. Environment Variables
+"""
+
+# =============================
+# 1. Imports
+# =============================
+# Standard library imports
+from os import environ
+
+
+# =============================
+# 2. Environment Variables
+# =============================
+# Set number of threads for OpenBLAS
+environ["OPENBLAS_NUM_THREADS"] = "1"
