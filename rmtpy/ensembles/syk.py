8188eb76e6146e8a3b3bc1c7eafad4b04f2f3dd1
Joshua Leeman <joshua.r.leeman@gmail.com>
2025-04-14 11:24:32 -0400

    Add SYK ensemble implementation with class definition and initialization

---
+++ rmtpy/ensembles/syk.py
@@ -0,0 +1,79 @@
+# rmtpy.ensembles.syk.py
+"""
+This module contains the programs defining the Sachdev-Ye-Kitaev (SYK) ensemble.
+It is grouped into the following sections:
+    1. Imports
+    2. Attributes
+    3. Ensemble Class
+"""
+
+
+# =============================
+# 1. Imports
+# =============================
+# Standard library imports
+from math import comb
+
+# Third-party imports
+import numpy as np
+
+# Local application imports
+from ._rmt import Ensemble
+
+
+# =============================
+# 2. Attributes
+# =============================
+# Class name for dynamic imports
+class_name = "SYK"
+
+
+# =============================
+# 2. Ensemble Class
+# =============================
+class SYK(Ensemble):
+    def __init__(
+        self,
+        q: int,
+        N: int,
+        scale: float = 1.0,
+        dtype: type = np.complex128,
+    ):
+        # Set SYK-specific parameters
+        self._q = q
+
+        # Initialize ensemble class
+        super().__init__(N=N, scale=scale, dtype=dtype)
+
+        # Check if SYK parameters are valid
+        self._check_ensemble()
+
+        # Calculate suppression factor
+        self._eta = np.sum(
+            (-1) ** (self.q - k)
+            * comb(self.q, k)
+            * comb(self.N - k, self.q - k)
+            / comb(self.N, self.q)
+            for k in range(self.q + 1)
+        )
+
+        # Determine Dyson index
+        self._beta = (
+            {(0, 0): 1, (0, 4): 4}.get((self.q % 4, self.N % 8), 2) if q > 2 else 0
+        )
+
+        # Calculate standard deviation of matrix elements
+        self._sigma = self.scale * np.sqrt((1 - self._eta) / comb(self.N, self.q)) / 2
+
+        # Set order of SYK arguments
+        self._arg_order = ["name", "q", "N", "scale"]
+
+    def _check_ensemble(self):
+        pass
+
+    @property
+    def q(self):
+        """
+        Get the SYK parameter q.
+        """
+        return self._q
