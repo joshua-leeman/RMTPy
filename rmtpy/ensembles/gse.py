3fd473f8985d6a1104c297f5a3dd9c1f7d04aeb7
Joshua Leeman <joshua.r.leeman@gmail.com>
2025-04-21 15:16:37 -0400

    Remove sigma attribute and related calculations from GSE class

--- rmtpy/ensembles/gse.py
+++ rmtpy/ensembles/gse.py
@@ -38,11 +38,6 @@ class GSE(Tenfold):
     The Gaussian Symplectic Ensemble (GSE) class.
     Inherits from the Tenfold class.
 
-    Attributes
-    ----------
-    sigma : float
-        Standard deviation of the matrix elements
-
     Methods
     -------
     generate() -> np.ndarray
@@ -75,9 +70,6 @@ class GSE(Tenfold):
 
         # Initialize tenfold ensemble
         super().__init__(beta=beta, N=N, dim=dim, J=J, dtype=dtype)
-
-        # Calculate standard deviation of matrix element parts
-        self._sigma = self.N * self.J / 2 / np.sqrt(4 * self.dim)
 
     def generate(self) -> np.ndarray:
         """
@@ -127,15 +119,8 @@ class GSE(Tenfold):
         # Write bottom-right block as complex conjugate of top-left block
         np.conjugate(H[:bdim, :bdim], out=H[bdim:, bdim:])
 
-        # Halve and scale matrix by standard deviation in place
-        H *= self.sigma / 2
+        # Halve and scale matrix by complex standard deviation in place
+        H *= self.sigma / 2 / np.sqrt(2)
 
         # Return GSE matrix
         return H
-
-    @property
-    def sigma(self) -> float:
-        """
-        Standard deviation of the matrix elements.
-        """
-        return self._sigma
