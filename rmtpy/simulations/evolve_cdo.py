d6b2c517f27dd1f249759cd3b880f6ff6f7ab2ef
Joshua Leeman <joshua.r.leeman@gmail.com>
2025-04-23 16:41:35 -0400

    Add module docstring for evolve_cdo.py with section overview

---
+++ rmtpy/simulations/evolve_cdo.py
@@ -0,0 +1,9 @@
+# rmtpy.simulations.evolve_cdo.py
+"""
+This module contains programs for performing Monte Carlo simulations to obtain the time evolutiovn of chaotic density operators (CDOs).
+It is grouped into the following sections:
+    1. Imports
+    2. Plotting Functions
+    3. Evolve CDO Class
+    4. Main Function
+"""
