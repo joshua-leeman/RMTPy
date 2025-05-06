203f2a2bb8c0e4f406003c06c7bf9529ed2cf1d2
Joshua Leeman <joshua.r.leeman@gmail.com>
2025-05-05 22:05:14 -0400

    Refactor syk.py: simplify class structure and improve implementation of the SYK ensemble

--- rmtpy/ensembles/syk.py
+++ rmtpy/ensembles/syk.py
@@ -1,287 +1,179 @@
 # rmtpy.ensembles.syk.py
-"""
-This module contains the programs defining the Sachdev-Ye-Kitaev (SYK) ensemble.
-It is grouped into the following sections:
-    1. Imports
-    2. Attributes
-    3. Ensemble Class
-"""
 
 
-# =============================
+# =======================================
 # 1. Imports
-# =============================
+# =======================================
 # Standard library imports
+from __future__ import annotations
 import itertools
+from dataclasses import dataclass, field
 from math import comb
-from typing import List
+from typing import Optional
 
 # Third-party imports
 import numpy as np
-from scipy.sparse import csr_matrix, eye_array, kron
+from scipy.sparse import csr_matrix
 
 # Local application imports
-from ._rmt import Ensemble
+from rmtpy.ensembles._rmt import ManyBodyEnsemble
+from rmtpy.special import create_majorana_pairs
 
 
-# =============================
-# 2. Attributes
-# =============================
-# Class name for dynamic imports
+# =======================================
+# 2. Ensemble
+# =======================================
+# Store class name for module
 class_name = "SYK"
 
 
-# =============================
-# 3. Ensemble Class
-# =============================
-class SYK(Ensemble):
-    """
-    The Sachdev-Ye-Kitaev (SYK) ensemble class.
-    Inherits from the Ensemble class.
+# Define Sachdev-Ye-Kitaev ensemble
+@dataclass(repr=False, eq=False, frozen=True, kw_only=True, slots=True)
+class SYK(ManyBodyEnsemble):
+    """The Sachdev-Ye-Kitaev model class."""
 
-    Attributes
-    ----------
-    q : int
-        Number of Majorana fermions.
-    N : int
-        Number of Majorana fermions.
-    J : float
-        Energy scale of interactions (default is 1.0).
-    sigma : float
-        Standard deviation of the couplings.
+    # SYK q-parameter
+    q: int
 
-    Methods
-    -------
-    generate() -> np.ndarray
-        Generate a random SYK Hamiltonian.
-    spectral_density(eigval: float, num_terms: int = 1000) -> float
-        Calculate the mean spectral density at a given eigenvalue.
-    """
+    # Suppression factor
+    eta: Optional[float] = field(init=False, default=None)
 
-    def __init__(
-        self,
-        q: int = None,
-        N: int = None,
-        J: float = 1.0,
-        dtype: type = np.complex128,
-    ) -> None:
-        """
-        Intialize the SYK ensemble.
+    # Standard deviation of couplings
+    sigma: Optional[float] = field(init=False, default=None)
 
-        Parameters
-        ----------
-        q : int
-            Number of Majorana fermions.
-        N : int
-            Number of Majorana fermions.
-        J : float, optional
-            Energy scale of interactions (default is 1.0).
-        dtype : type, optional
-            Data type of the matrix elements (default is np.complex128).
-        """
-        # Set SYK q-parameter
-        self._q = q
+    # Majorana fermion operators
+    majorana_pairs: Optional[tuple[tuple[Optional[csr_matrix], ...], ...]] = field(
+        init=False, default=None
+    )
 
-        # Determine Dyson index
-        self._beta = {(0, 0): 1, (0, 4): 4}.get((q % 4, N % 8), 2) if q > 2 else 0
+    # Default ensemble argument names
+    _ens_args: tuple[str, str] = field(init=False, default=("q", "N", "J", "dtype"))
 
-        # Set degeneracy of eigenvalues
-        self._degen = 2 if self.beta == 4 else 1
+    def __post_init__(self) -> None:
+        # Create map to determine SYK Dyson index
+        index = {(0, 0): 1, (0, 4): 4}.get((self.q % 4, self.N % 8), 2)
+
+        # Set SYK ensemble's Dyson index
+        object.__setattr__(self, "beta", index if self.q > 2 else 0)
 
         # Calculate suppression factor
-        self._eta = np.sum(
-            (-1) ** (q - k) * comb(q, k) * comb(N - q, q - k) for k in range(q + 1)
-        ) / comb(N, q)
+        eta = np.sum(
+            (-1) ** (self.q - k) * comb(self.q, k) * comb(self.N - self.q, self.q - k)
+            for k in range(self.q + 1)
+        ) / comb(self.N, self.q)
 
-        # Calculate standard deviation of couplings
-        self._sigma = N * J * np.sqrt((1 - self.eta) / comb(N, q)) / 2
+        # Set suppression factor
+        object.__setattr__(self, "eta", eta)
 
-        # Initialize ensemble class
-        super().__init__(N=N, J=J, dtype=dtype)
+        # Calculate standard deviation
+        sigma = self.N * self.J * np.sqrt((1 - eta) / comb(self.N, self.q)) / 2
 
-        # Set order of SYK arguments
-        self._arg_order = ["name", "q", "N", "J"]
+        # Set standard deviation
+        object.__setattr__(self, "sigma", sigma)
 
-    def __repr__(self) -> str:
-        """
-        String representation of the SYK ensemble.
-        """
-        return f"{self.__class__.__name__}(q={self.q}, N={self.N}, J={self.J})"
+        # Finish initialization of ManyBodyEnsemble instance
+        super(SYK, self).__post_init__()
 
-    def __str__(self) -> str:
-        """
-        LaTeX representation of the SYK ensemble.
-        """
-        return rf"$\textrm{{{self.__class__.__name__}}}_{self.q}\ N={self.N}$"
+        # Residual memory is required to store the Majorana pairs in bytes
+        object.__setattr__(self, "resid_memory", comb(self.N, 2) * (24 * self.dim + 6))
 
-    def _check_ensemble(self) -> None:
-        """
-        Check if the SYK parameters are valid.
+    def randm(self, out: Optional[np.ndarray] = None) -> None:
+        """Generate a random matrix from the SYK ensemble."""
+        # If out is None, create a new zeroed array
+        if out is None:
+            H = np.zeros((self.dim, self.dim), dtype=self.dtype, order="F")
+        # If out is provided, zero it
+        else:
+            H = out
+            H[:, :] = 0.0
 
-        Raises
-        ------
-        ValueError
-            If the SYK parameters are invalid.
-        """
-        # Check if base ensemble parameters are valid
-        super()._check_ensemble()
-
-        # Checks if SYK q-parameter is provided
-        if self.q is None:
-            raise ValueError("SYK q-parameter must be provided.")
-
-        # Check if SYK parameters are valid
-        if self.q < 2 or self.q % 2 != 0:
-            raise ValueError(
-                f"Invalid SYK q-parameter: q={self.q}. Must be even and greater than or equal to 2."
-            )
-        elif self.N <= self.q:
-            raise ValueError(f"Invalid N: N={self.N}. Must be greater than q={self.q}.")
-
-    def _create_majoranas(self) -> List[csr_matrix]:
-        """
-        Create Majorana operators for the SYK ensemble.
-
-        Returns
-        -------
-        List[csr_matrix]
-            List of Majorana operators as sparse matrices.
-        """
-        # Create Pauli matrices
-        pauli = [
-            csr_matrix([[0, 1], [1, 0]], dtype=self.dtype),  # sigma_x
-            csr_matrix([[0, -1j], [1j, 0]], dtype=self.dtype),  # sigma_y
-            csr_matrix([[1, 0], [0, -1]], dtype=self.dtype),  # sigma_z
-        ]
-
-        # Create initial Majorana operators
-        majorana_0 = pauli[:2]
-        majorana_c0 = pauli[2]
-
-        # With loop, build Majorana operators from the initial ones
-        for i in range(self.N // 2 - 1):
-            # Create identity matrix corresponding to old Majorana operators
-            eye_mat = eye_array(2 ** (i + 1), format="csr", dtype=self.dtype)
-
-            # Initialize new Majorana operators
-            majorana = [None for _ in range(len(majorana_0) + 2)]
-
-            # Create new Majorana operators
-            for j in range(len(majorana_0)):
-                majorana[j] = kron(pauli[0], majorana_0[j], format="csr")
-            majorana[-2] = kron(pauli[0], majorana_c0, format="csr")
-            majorana[-1] = kron(pauli[1], eye_mat, format="csr")
-
-            # If not the last Majorana operators, update new as old
-            # Else, return the last Majorana operators
-            if i < self.N // 2 - 2:
-                majorana_0 = majorana
-                majorana_c0 = kron(pauli[2], eye_mat, format="csr")
-            else:
-                return majorana
-
-    def generate(self) -> np.ndarray:
-        """
-        Return a random SYK Hamiltonian.
-
-        Returns
-        -------
-        np.ndarray
-            Random SYK Hamiltonian as a dense matrix.
-        """
         # Create Majorana operators if not already created
-        if not hasattr(self, "_majorana"):
-            self._majorana = self._create_majoranas()
+        if self.majorana_pairs is None:
+            object.__setattr__(self, "majorana_pairs", create_majorana_pairs(N=self.N))
 
         # Pre-draw all random coefficients
         coeffs = self._rng.standard_normal(
             size=comb(self.N, self.q), dtype=self.real_dtype
         )
 
-        # Initialize sparse SYK Hamiltonian
-        H = csr_matrix((self.dim, self.dim), dtype=self.dtype)
+        # Retrieve indices for Hamiltonian terms
+        indices = tuple(itertools.combinations(range(self.N), self.q))
 
         # Generate and sum q-body operators
-        for coeff, idx_tuple in zip(
-            coeffs, itertools.combinations(range(self.N), self.q)
-        ):
-            # Build q-body operator
-            q_body = self.majorana[idx_tuple[0]]
-            for idx in idx_tuple[1:]:
-                q_body = q_body.dot(self.majorana[idx])
+        for coeff, idx_tuple in zip(coeffs, indices):
+            # Divide indices into pairs
+            pairs = tuple((idx_tuple[i], idx_tuple[i + 1]) for i in range(0, self.q, 2))
 
-            # Add to Hamiltonian
-            H += coeff * q_body[: self.dim, : self.dim]
+            # Start q-body operator with first pair
+            j0, k0 = pairs[0]
+            q_body = self.majorana_pairs[j0][k0]
+
+            # Multiply with remaining pairs
+            for j, k in pairs[1:]:
+                q_body = q_body.dot(self.majorana_pairs[j][k])
+
+            # Store q-body operator as COO matrix
+            q_coo = q_body[: self.dim, : self.dim].tocoo()
+
+            # Add q-body operator to Hamiltonian
+            H[q_coo.row, q_coo.col] += coeff * q_coo.data
 
         # Scale Hamiltonian by standard deviation and global phase
         H *= 1j ** (self.q * (self.q - 1) / 2) * self.sigma
 
-        # Return SYK Hamiltonian as a dense matrix
-        return H.toarray()
+        # Return SYK Hamiltonian
+        return H
 
-    def spectral_density(self, eigval: float, num_terms: int = 100) -> float:
-        """
-        Calculate the mean spectral density at a given eigenvalue.
+    def pdf(self, eigval: np.ndarray, num_terms: int = 100) -> float:
+        """SYK average spectral density."""
+        # Initialize probability distribution function (PDF) array
+        pdf = np.zeros(eigval.shape, dtype=self.real_dtype)
 
-        Parameters
-        ----------
-        eigval : float
-            Eigenvalue at which to calculate the mean spectral density.
-
-        Returns
-        -------
-        float
-            Mean spectral density at the given eigenvalue.
-        """
-        # Return zero if eigenvalue is outside support
-        if abs(eigval) > self._E0:
-            return 0.0
+        # Create mask for non-trivial PDF values
+        mask = np.abs(eigval) < self.E0
 
         # Create vector of term indices
         k = np.arange(num_terms)
 
         # term1 = 1 - (2E/E0)**2 * eta**(k+1)/(1 + eta**(k+1))**2
         etak1 = self.eta ** (k + 1)
-        term1 = 1 - (2 * eigval / self._E0) ** 2 * etak1 / (1.0 + etak1) ** 2
+        eigval_sq = (eigval[mask] ** 2)[:, None]
+        term1 = 1 - (4 * eigval_sq / self.E0**2) * etak1 / (1.0 + etak1) ** 2
 
         # term2 = (1 - eta**(2*k+2))/(1 - eta**(2*k+1))
         term2 = (1.0 - self.eta ** (2 * k + 2)) / (1.0 - self.eta ** (2 * k + 1))
 
         # Sum logarithm of terms
-        logP = np.log(term1) + np.log(term2)
-        logP = np.sum(logP) + 0.5 * np.log(1.0 - self.eta)
+        logP = np.log(term1) + np.log(term2)[None, :]
+        logP = np.sum(logP, axis=1) + 0.5 * np.log(1.0 - self.eta)
 
         # Construct product and final result
-        P = np.exp(logP) * np.sqrt(1.0 - (eigval / self._E0) ** 2)
+        P = np.exp(logP) * np.sqrt(1.0 - (eigval[mask] / self.E0) ** 2)
         P /= np.pi * np.sqrt(comb(self.N, self.q)) * self.sigma
 
-        # Return mean spectral density at eigenvalue
-        return P
+        # Store non-trivial PDF values
+        pdf[mask] = P
 
-    @property
-    def q(self):
-        """
-        Get the SYK parameter q.
-        """
-        return self._q
+        # Return PDF values
+        return pdf
 
-    @property
-    def eta(self):
-        """
-        Get the suppression factor.
-        """
-        return self._eta
+    def _check_ensemble(self) -> None:
+        """Check if the SYK ensemble is valid."""
+        # Check if instance is valid ManyBodyEnsemble instance
+        super(SYK, self)._check_ensemble()
 
-    @property
-    def sigma(self):
-        """
-        Standard deviation of the couplings.
-        """
-        return self._sigma
+        # Check if SYK q-parameter is provided
+        if self.q is None:
+            raise ValueError("SYK q-parameter must be provided.")
 
-    @property
-    def majorana(self):
-        """
-        Majorana operators.
-        """
-        return self._majorana
+        # Check if SYK parameters are valid
+        if not isinstance(self.q, int) or self.q < 2 or self.q % 2 != 0:
+            raise ValueError(f"SYK q-parameter must be an even integer.")
+
+        elif self.N <= self.q:
+            raise ValueError("Number of Majoranas must be greater than q-parameter.")
+
+    def _to_latex(self) -> str:
+        """LaTeX representation of the ManyBodyEnsemble."""
+        # Return formatted LaTeX string
+        return rf"$\textrm{{{self.__class__.__name__}}}_{self.q}\ N={self.N}$"
