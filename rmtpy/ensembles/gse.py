9c405f75ac248d5d71d68a34b7375d463b1723b6
Joshua Leeman <joshua.r.leeman@gmail.com>
2025-05-05 22:05:05 -0400

    Refactor gse.py: simplify class structure and improve random matrix generation for Gaussian Symplectic Ensemble (GSE)

--- rmtpy/ensembles/gse.py
+++ rmtpy/ensembles/gse.py
@@ -1,90 +1,46 @@
 # rmtpy.ensembles.gse.py
-"""
-This module contains the programs defining the Gaussian Symplectic Ensemble (GSE).
-It is grouped into the following sections:
-    1. Imports
-    2. Attributes
-    3. Ensemble Class
-"""
 
-# =============================
+
+# =======================================
 # 1. Imports
-# =============================
+# =======================================
+# Standard library imports
+from __future__ import annotations
+from dataclasses import dataclass, field
+from typing import Optional
+
 # Third-party imports
 import numpy as np
 
 # Local application imports
-from ._rmt import Tenfold
+from rmtpy.ensembles._rmt import GaussianEnsemble
 
 
-# =============================
-# 2. Attributes
-# =============================
-# Class name for dynamic imports
+# =======================================
+# 2. Ensemble
+# =======================================
+# Store class name for module
 class_name = "GSE"
 
-# Dyson index
-beta = 4
 
-# Degeneracy of eigenvalues
-degen = 2
+# Define Gaussian Symplectic Ensemble (GSE) class
+@dataclass(repr=False, eq=False, frozen=True, kw_only=True, slots=True)
+class GSE(GaussianEnsemble):
+    """Gaussian Symplectic Ensemble (GSE) class."""
 
+    # Dyson index
+    beta: int = field(init=False, default=4)
 
-# =============================
-# 3. Ensemble Class
-# =============================
-class GSE(Tenfold):
-    """
-    The Gaussian Symplectic Ensemble (GSE) class.
-    Inherits from the Tenfold class.
+    def randm(self, out: Optional[np.ndarray] = None) -> np.ndarray:
+        """Generate a random matrix from the GSE."""
+        # If out is None, allocate memory for matrix
+        if out is None:
+            H = np.empty((self.dim, self.dim), dtype=self.dtype, order="F")
+        else:
+            H = out
 
-    Methods
-    -------
-    generate() -> np.ndarray
-        Generate a random matrix from the GSE.
-    """
-
-    def __init__(
-        self,
-        N: int = None,
-        dim: int = None,
-        J: float = 1.0,
-        dtype: type = np.complex128,
-    ) -> None:
-        """
-        Initialize the Gaussian Symplectic Ensemble (GSE).
-
-        Parameters
-        ----------
-        N : int, optional
-            Number of Majorana fermions
-        dim : int, optional
-            Dimension of the matrix
-        J : float, optional
-            Energy scale of interactions (default is 1.0)
-        dtype : type, optional
-            Data type of the matrix elements (default is np.complex128)
-        """
-        # Set degeneracy of eigenvalues
-        self._degen = degen
-
-        # Initialize tenfold ensemble
-        super().__init__(beta=beta, N=N, dim=dim, J=J, dtype=dtype)
-
-    def generate(self) -> np.ndarray:
-        """
-        Return a random matrix from the GSE.
-
-        Returns
-        -------
-        np.ndarray
-            Random matrix from the GSE.
-        """
         # Compute block dimension
         bdim = self.dim // 2
-
-        # Allocate memory for GSE matrix
-        H = np.empty((self.dim, self.dim), dtype=self.dtype)
 
         # Generate GUE in top-left block
         H[:bdim, :bdim].real = self._rng.standard_normal(
