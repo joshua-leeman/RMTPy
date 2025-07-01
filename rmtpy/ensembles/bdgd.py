af20f0f0c6564f6ac26e5f3608e287c9d02859f8
Joshua Leeman <joshua.r.leeman@gmail.com>
2025-07-01 18:46:25 -0400

    Refactor BdGD class: update frozen decorator options for improved behavior

--- rmtpy/ensembles/bdgd.py
+++ rmtpy/ensembles/bdgd.py
@@ -14,7 +14,7 @@ from ._manybody import GaussianEnsemble
 # ----------------------------------------
 # Bogoliubov-de Gennes D Ensemble (BdG(D))
 # ----------------------------------------
-@frozen(kw_only=True, eq=False, unsafe_hash=False)
+@frozen(kw_only=True, eq=False, weakref_slot=False, getstate_setstate=False)
 class BdGD(GaussianEnsemble):
     @property
     def beta(self: BdGD) -> int:
