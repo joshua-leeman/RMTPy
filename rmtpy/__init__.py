59e9bc05814aa460a770dbba1159a49560c39074
Joshua Leeman <joshua.r.leeman@gmail.com>
2025-10-13 15:07:09 -0400

    Refactor __init__.py: remove unused imports and global namespace updates for simulations

--- rmtpy/__init__.py
+++ rmtpy/__init__.py
@@ -1,24 +1,8 @@
 # rmtpy/__init__.py
 
-# Standard library imports
-from typing import Any as __Any
-
 # Local application imports
 from .ensembles import ens_dict as __ens_dict, ensemble
-from .simulations import (
-    sim_dict as __sim_dict,
-    sim_exe_dict as __sim_exe_dict,
-    Simulation as __Simulation,
-)
-
-# Update global namespace registered ensembles
-globals().update(__ens_dict)
-
-# Update global namespace with registered simulations
-globals().update(__sim_dict)
-
-# Update global namespace with registered simulation executables
-globals().update(__sim_exe_dict)
+from .simulations import sim_dict as __sim_dict, sim_exe_dict as __sim_exe_dict
 
 # Redefine __all__ to include all registered ensembles
 __all__ = (
