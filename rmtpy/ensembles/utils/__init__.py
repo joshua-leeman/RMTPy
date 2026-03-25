0b8732a7c5ae336df91759ba1518883473e3d82b
Joshua Leeman <joshua.r.leeman@gmail.com>
2026-03-25 18:27:26 -0400

    Add to_real_basis function for direct access

--- rmtpy/ensembles/utils/__init__.py
+++ rmtpy/ensembles/utils/__init__.py
@@ -1,4 +1,4 @@
 # rmtpy/ensembles/utils/__init__.py
 
 # Local application imports
-from .fermions import create_majoranas, create_majorana_pairs
+from .fermions import create_majoranas, create_majorana_pairs, to_real_basis
