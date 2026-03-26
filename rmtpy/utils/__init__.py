1408ae71bc14aecc594977e0e4b0dd41f1e386cf
Joshua Leeman <joshua.r.leeman@gmail.com>
2026-03-26 10:51:04 -0400

    Access more functions from fermion submodle in utils

--- rmtpy/utils/__init__.py
+++ rmtpy/utils/__init__.py
@@ -1,4 +1,10 @@
 # rmtpy/utils/__init__.py
 
 # Local application imports
-from .fermions import create_majoranas, create_majorana_pairs
+from .fermions import (
+    block_slice,
+    create_complex_fermions,
+    create_majoranas,
+    create_majorana_pairs,
+    vacuum_state,
+)
