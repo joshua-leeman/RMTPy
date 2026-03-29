2ca79f1812f6f36a626daff37f00cf3686538714
Joshua Leeman <joshua.r.leeman@gmail.com>
2026-03-29 17:43:23 -0400

    Rewrite Compound ensemble class, define S-matrix and Q-matrix iterators

--- rmtpy/compounds/_compound.py
+++ rmtpy/compounds/_compound.py
@@ -5,22 +5,56 @@ from __future__ import annotations
 
 # Standard library imports
 import math
+from abc import ABC, abstractmethod
+
+# Third-party imports
+import numpy as np
+from attrs import Converter, field, frozen
+from attrs.validators import instance_of
+from scipy.linalg import eigvals, solve
 
 # Local application imports
 from ..ensembles import ManyBodyEnsemble
 from collections.abc import Iterator
 
-# Third-party imports
-import numpy as np
-from attrs import field, frozen
-from attrs.validators import instance_of
+
+# -----------------------------
+# Couplings Strengths Converter
+# -----------------------------
+def __to_full_array(value: float | np.ndarray, self_: Compound) -> np.ndarray:
+    """If value is a scalar, create an array of coupling strengths with the same value for all entries."""
+
+    # Alias number of open channels
+    channels = self_.channels
+
+    # =================================================
+
+    # If value is a scalar, set all couplings to the same value
+    if isinstance(value, (int, float)):
+        return np.full(channels, value, order="F")
+
+    # If value is an array, ensure it has the correct shape
+    elif isinstance(value, np.ndarray):
+        if value.shape != (channels,):
+            raise ValueError(
+                f"Coupling strengths array must have shape ({channels},), got {value.shape}."
+            )
+
+        # Return array of coupling strengths in Fortran order
+        return np.asfortranarray(value)
+
+    # If value is neither a scalar nor an array, raise an error
+    else:
+        raise TypeError(
+            f"Coupling strengths must be a scalar or a numpy array, got {type(value).__name__}."
+        )
 
 
 # ---------------------------
 # Many-body Compound Ensemble
 # ---------------------------
 @frozen(kw_only=True, eq=False, weakref_slot=False, getstate_setstate=False)
-class Compound:
+class Compound(ABC):
 
     # Underlying RMT ensemble
     ensemble: ManyBodyEnsemble = field(validator=instance_of(ManyBodyEnsemble))
@@ -32,21 +66,21 @@ class Compound:
     channels: int = field(init=False)
 
     # Coupling strengths
-    couplings: float = field()
+    strengths: np.ndarray = field(converter=Converter(__to_full_array, takes_self=True))
 
     @fermions.validator
     def __fermions_validator(self, _, value: int) -> None:
-        """Ensure number of fermions is a positive integer."""
+        """Ensure number of fermions is a nonnegative integer."""
 
         # Alias number of complex fermions in ensemble
-        Nc = self.ensemble.N // 2
+        Nc = self.ensemble.Nm // 2
 
         # =================================================
 
-        # Ensure number of fermions is a positive integer less than or equal to Nc
-        if value <= 0 or value > Nc:
+        # Ensure number of fermions is nonnegative and less than or equal to Nc
+        if value < 0 or value > Nc:
             raise ValueError(
-                f"Number of fermions must be a positive integer less than or equal to {Nc}, got {value}."
+                f"Number of fermions must be a nonnegative integer less than or equal to {Nc}, got {value}."
             )
 
     @channels.default
@@ -57,9 +91,134 @@ class Compound:
         k = self.fermions
 
         # Alias number of complex fermions in ensemble
-        Nc = self.ensemble.N // 2
+        Nc = self.ensemble.Nm // 2
 
         # =================================================
 
         # Calculate and return number of combinations
         return math.comb(Nc, k)
+
+    @strengths.default
+    def __strengths_default(self) -> float:
+        """Set uniform strengths to square root of ground state energy."""
+
+        # Alias ensemble ground state energy
+        E0 = self.ensemble.E0
+
+        # =================================================
+
+        # Set uniform strengths to square root of ground state energy
+        return math.sqrt(E0)
+
+    def resonance_stream(self, realizs: int) -> Iterator[np.ndarray]:
+        """Iterator to stream effective Hamiltonian eigenvalues (resonances) realizations."""
+
+        # Alias ensemble dimension
+        dim = self.ensemble.dim
+
+        # Alias ensemble data type
+        dtype = self.ensemble.dtype
+
+        # =================================================
+
+        # Allocate memory for random effective Hamiltonians
+        H_eff = np.empty((dim, dim), dtype=dtype, order="F")
+
+        # Loop over realizations . . .
+        for _ in range(realizs):
+            # Generate effective Hamiltonian
+            self.generate_H_eff(out=H_eff)
+
+            # Compute and yield effective Hamiltonian eigenvalues
+            yield eigvals(H_eff, overwrite_a=True, check_finite=False)
+
+    def S_matrix_stream(
+        self, energies: np.ndarray, realizs: int
+    ) -> Iterator[np.ndarray]:
+        """Iterator to stream S matrix realizations at given energies."""
+
+        # Ensure energies is a 1D array
+        if energies.ndim != 1:
+            raise ValueError(f"Energies must be 1D array, got {energies.ndim}D array.")
+
+        # Alias number of energies
+        num = energies.size
+
+        # Alias ensemble data type
+        dtype = self.ensemble.dtype
+
+        # Alias number of open channels
+        L = self.channels
+
+        # =================================================
+
+        # Allocate memory for coefficient matrix conjugate
+        Cc = np.empty((num, L, L), dtype=dtype, order="C")
+
+        # From K-matrix realizations . . .
+        for C in self.K_matrix_stream(energies, realizs):
+
+            # Calculate coefficient matrix
+            C *= 1j * np.pi
+            C.diagonal(axis1=-2, axis2=-1)[:] += 1
+
+            # Store conjugate of coefficient matrix in Cc
+            np.conjugate(C, out=Cc)
+
+            # Yield S-matrix realization
+            yield solve(C, Cc, overwrite_a=True, overwrite_b=True, check_finite=False)
+
+    def Q_matrix_stream(
+        self, energies: np.ndarray, realizs: int
+    ) -> Iterator[np.ndarray]:
+        """Iterator to stream Q matrix realizations at given energies."""
+
+        # Ensure energies is a 1D array
+        if energies.ndim != 1:
+            raise ValueError(f"Energies must be 1D array, got {energies.ndim}D array.")
+
+        # =================================================
+
+        # From K, K_2 matrix realizations . . .
+        for C, K_2 in self.K_K2_matrix_stream(energies, realizs):
+
+            # Calculate coefficient matrix
+            C *= -1j * np.pi
+            C.diagonal(axis1=-2, axis2=-1)[:] += 1
+
+            # Scale K_2 matrix by pi
+            K_2 *= np.pi
+
+            # Calculate Q-matrix realization
+            Q = solve(C, K_2, overwrite_a=True, overwrite_b=True, check_finite=False)
+
+            # Add its conjugate transpose to itself in place
+            Q += Q.swapaxes(-1, -2).conj()
+
+            # Yield Q-matrix realization
+            yield Q
+
+    @abstractmethod
+    def generate_H_eff(self) -> np.ndarray:
+        """Generate a random effective Hamiltonian."""
+
+        # This method should be implemented by subclasses
+        pass
+
+    @abstractmethod
+    def K_matrix_stream(
+        self, energies: np.ndarray, realizs: int
+    ) -> Iterator[np.ndarray]:
+        """Iterator to stream K matrix realizations at energies."""
+
+        # This method should be implemented by subclasses
+        pass
+
+    @abstractmethod
+    def K_K2_matrix_stream(
+        self, energies: np.ndarray, realizs: int
+    ) -> Iterator[tuple[np.ndarray, np.ndarray]]:
+        """Iterator to stream K and K_2 matrix realizations at energies."""
+
+        # This method should be implemented by subclasses
+        pass
